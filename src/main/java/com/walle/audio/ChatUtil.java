cf964cf453cedad1c62a1432d17132f0fdfb4778
dingxl <dingxl>
2020-03-19 14:06:21 +0800

    close #5 客户端读取ip，调用后台时传入参数

--- src/main/java/com/walle/audio/ChatUtil.java
+++ src/main/java/com/walle/audio/ChatUtil.java
@@ -1,6 +1,8 @@
 package com.walle.audio;
 
+import com.alibaba.fastjson.JSONObject;
 import com.walle.http.HttpUtil;
+import com.walle.http.LocationUtil;
 import com.walle.http.RespData;
 import com.walle.util.B64Util;
 import com.walle.util.MacUtil;
@@ -12,6 +14,7 @@ public class ChatUtil {
     public static void chat(TimeListener listener) {
         RecordHelper recordHelper = RecordHelper.getInst();
         final ByteArrayOutputStream data = recordHelper.save(new ByteArrayOutputStream());
+        final JSONObject location = LocationUtil.getLocation();
 
         RespData resp = new RespData();
         byte[] ret = HttpUtil.sendHttpPost(
@@ -21,6 +24,7 @@ public class ChatUtil {
                     put("format", "wav");
                     put("uid", MacUtil.gtMacAddr());
                     put("audio", B64Util.encode(data.toByteArray()));
+                    put("ip", location == null ? "" : location.getString("ip"));
                 }}, resp
         );
         System.out.printf("%s, %s, %s, %d, %d\n",
